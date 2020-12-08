5a567ea202efc21b72154cae6c51f278aeede04b
zaycation.dev <isaiahthomas098@gmail.com>
2020-12-08 04:10:24 -0800

    oauth2

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: `418924332805825`,
+    clientSecret: `a69fc58d028ffddf223dd84438d07132`,
+  },
 };
